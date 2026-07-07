16bc6e275ad435e1b9db74a5a1cdd4615c57d435
Ettienne Janse van Vuuren <ettienne.jvv@icloud.com>
2026-07-07 11:18:37 +0200

    Set up Express backend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require("express")
+const config = require("./config/config")
 
 const app = express()
 
-const PORT = 3000
 
 app.get("/",function(request,response){
     response.send("Welcome to the AquaTrack API!")
 })
 
-app.listen(PORT,function(){
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.get("/api/health",function(request,response){
+    response.json({
+        status: "ok",
+        message: "AquaTrack API is runnning"
+    })
+})
+app.listen(config.PORT,function(){
+    console.log(`Server is running on http://localhost:${config.PORT}`)
 })
